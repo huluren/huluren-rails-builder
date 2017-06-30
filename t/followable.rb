28acb30c2b2696bf68c56b35ffe4d23f66d28c28
Liu Lantao <Lax@users.noreply.github.com>
2017-06-30 13:21:18 +0800

    Update followable.rb

--- t/followable.rb
+++ t/followable.rb
@@ -10,3 +10,29 @@ inside 'app/models/' do
   CODE
 
 end
+
+inside 'spec/' do
+  gsub_file 'factories/follows.rb', /(^\s*?)(follower|followable) nil$/, '\1association :\2, factory: :user'
+
+  gsub_file 'models/follow_spec.rb', /(^(\s*)?)pending .*\n/, <<-CODE
+\\1it "should increment the count" do
+\\2  expect{ create(:follow) }.to change{Follow.count}.by(1)
+\\2end
+
+\\2it "can follow" do
+\\2  follower = create(:user)
+\\2  followable = create(:user)
+
+\\2  expect{ follower.follow(followable) }.to change{Follow.count}.by(1)
+\\2  expect( follower.follow?(followable) ).to be true
+\\2end
+
+\\2it "should fail without :follower" do
+\\2  expect( build(:follow, follower: nil) ).to be_invalid
+\\2end
+
+\\2it "should fail without :followable" do
+\\2  expect( build(:follow, followable: nil) ).to be_invalid
+\\2end
+  CODE
+end
