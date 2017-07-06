9c74df809be4530f1610fd2c0c4a54b0ae4a3682
Liu Lantao <Lax@users.noreply.github.com>
2017-07-06 12:05:35 +0800

    Update one.rb

--- one.rb
+++ one.rb
@@ -145,7 +145,7 @@ Rails.application.configure do
 end
   CODE
 
-  file 'timeago.rb, <<-CODE
+  file 'timeago.rb', <<-CODE
 Rails::Timeago.default_options limit: -> { 5.days.ago }, date_only: false, format: :short, nojs: false
   CODE
 
