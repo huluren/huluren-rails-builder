c1a70feb364f1a6ed052bb44a5f893e47e630c8e
Liu Lantao <Lax@users.noreply.github.com>
2017-07-10 23:50:28 +0800

    Update activity.rb

--- t/activity.rb
+++ t/activity.rb
@@ -42,7 +42,7 @@ end
 
 inside 'app/controllers/' do
   inject_into_class 'activities_controller.rb', ActivitiesController, <<-CODE
-before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   CODE
 
   gsub_file 'activities_controller.rb', /(\n(\s*?)def new\n[^\n]*?\n)(\s*?end)\n/m, <<-CODE
