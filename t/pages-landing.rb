76a4ed9d068bb10d531a3cda189316f4d2b28fd7
Liu Lantao <Lax@users.noreply.github.com>
2017-07-12 01:14:23 +0800

    Update pages-landing.rb

--- t/pages-landing.rb
+++ t/pages-landing.rb
@@ -6,9 +6,73 @@ inside('app/views/pages/') do
   remove_file 'landing.html.haml'
 
   file 'landing.html.haml', <<-CODE
-.d-flex.justify-content-center<>
-  %h1 Think different.
+.row.d-flex.justify-content-center<>
+  .articles.col-lg-3
+    = t('articles')
+  .col-lg-9.row.d-flex.flex-row-reverse
+    .app.col-md-2
+      = t('app')
+    .activities.col-md
+      = t('menu.activities')
+      #activities
+    .places.col-md
+      = t('menu.places')
+      #places
   CODE
+end
+
+inside('app/views/activities/') do
+  file 'index.js.coffee', <<-CODE
+$("#activities").replaceWith "<%= escape_javascript(render 'activities', items: @activities) %>"
+  CODE
+
+  file '_activities.html.haml', <<-CODE
+%ul#activities
+  - items.each do |activity|
+    %li
+      = activity.places.pluck(:name).to_sentence
+  CODE
+end
+
+inside('app/views/places/') do
+  file 'index.js.coffee', <<-CODE
+$("#places").replaceWith "<%= escape_javascript(render 'places', items: @places) %>"
+  CODE
+
+  file '_places.html.haml', <<-CODE
+%ul#places
+  - items.each do |place|
+    %li
+      = place.name
+  CODE
+end
+
+inside 'app/assets/javascripts/' do
+
+  append_to_file 'pages.coffee', <<-CODE
+$(document).on "turbolinks:load", ->
+  $("#activities").html("Loading activities...")
+  $("#places").html("Loading places...")
+
+  $.ajax
+    method: "GET"
+    url: '/places'
+    data:
+      c: 6
+      s: true
+    dataType: "script"
+
+  $.ajax
+    method: "GET"
+    url: '/activities'
+    data:
+      c: 6
+      s: true
+    dataType: "script"
+
+  true
+  CODE
+
 end
 
 inside('spec/views/pages/') do
