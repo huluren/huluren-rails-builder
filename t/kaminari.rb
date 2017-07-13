c58a731041a45cd38c78688247d86f6f902a0f1f
Liu Lantao <Lax@users.noreply.github.com>
2017-07-13 23:20:58 +0800

    Update kaminari.rb

--- t/kaminari.rb
+++ t/kaminari.rb
@@ -2,11 +2,11 @@ generate 'kaminari:views bootstrap4'
 
 inside 'app/controllers/' do
 
-  gsub_file 'activities_controller.rb', /(\n(\s*?)def index\n[^\n]*?Activity\..*)\n/m, <<-CODE
+  gsub_file 'activities_controller.rb', /(\n(\s*?)def index\n[^\n]*?Activity\.[^\n]*)\n/m, <<-CODE
 \\1.page(params[:page])
   CODE
 
-  gsub_file 'places_controller.rb', /(\n(\s*?)def index\n[^\n]*?Place\..*)\n/m, <<-CODE
+  gsub_file 'places_controller.rb', /(\n(\s*?)def index\n[^\n]*?Place\.[^\n]*)\n/m, <<-CODE
 \\1.page(params[:page])
   CODE
 
