043e5d873bf15033464b41ea16f947cea0a9a5b4
Liu Lantao <Lax@users.noreply.github.com>
2017-07-24 00:14:00 +0800

    Update puma.rb

--- t/puma.rb
+++ t/puma.rb
@@ -1,15 +1,17 @@
 file 'Procfile', 'web: bundle exec puma -C config/puma.rb'
 
 file 'config/puma.rb', <<-CODE
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+plugin :tmp_restart
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
