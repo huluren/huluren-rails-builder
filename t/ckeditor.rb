cf10c6e4ecaf62307d2e066174745b6268384a9c
Liu Lantao <Lax@users.noreply.github.com>
2017-07-23 13:48:51 +0800

    Update ckeditor.rb

--- t/ckeditor.rb
+++ t/ckeditor.rb
@@ -32,6 +32,21 @@ inside('app/views/activities/') do
   CODE
 end
 
+inside('app/controllers/') do
+  gsub_file 'activities_controller.rb', /^(\s+?)def new.*?\1[^\s]+\n/, <<-CODE
+\\0
+\\1def import
+\\1  @activity = Activity.new
+\\1  @activity.user = current_user
+\\1  @activity.schedules.new
+
+\\1  @activity.description = Nokogiri::HTML(open params[:url]).css("#content>h1, #content .article .topic-content .topic-doc .from a, #content .article .topic-content .topic-doc h3 span.color-green, #content .article .topic-content .topic-doc .topic-content")
+
+\\1  render :new
+\\1end
+  CODE
+end
+
 gsub_file 'config/routes.rb', /^(\s+?)resources :activities$/, <<-CODE
 \\0 do
 \\1  get 'import', on: :new
