ad7e850124b24ca51a064e5b2ba9eef528bc8d39
Liu Lantao <Lax@users.noreply.github.com>
2017-07-27 17:01:32 +0800

    Update activity.rb

--- t/activity.rb
+++ t/activity.rb
@@ -138,7 +138,6 @@ inside 'app/views/activities/' do
 
   gsub_file 'new.html.haml', /= link_to 'Back', .*$/, %q^= link_to t('action.back'), :back^
   gsub_file 'edit.html.haml', /= link_to 'Back', .*$/, %q^= link_to t('action.back'), :back^
-  gsub_file 'show.html.haml', /= link_to 'Back', .*$/, %q^= link_to t('action.back'), :back^
 end
 
 inside 'spec/factories/' do
