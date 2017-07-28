f485eec40c2625ae4ffae6a0e429d06ff1ef65ac
Liu Lantao <Lax@users.noreply.github.com>
2017-07-28 22:41:54 +0800

    Update models.rb

--- t/models.rb
+++ t/models.rb
@@ -1,3 +1,4 @@
 inject_into_class 'app/models/application_record.rb', 'ApplicationRecord', <<-CODE
+  scope :recent, -> { order(id: :desc) }
   scope :sample, ->(s=true) { s ? order("RANDOM()") : nil }
 CODE
