beecdb8d57abdc18a9dc5b62a23f47e5f7ac6be1
Liu Lantao <Lax@users.noreply.github.com>
2017-08-07 01:38:00 +0800

    Update ckeditor.rb

--- t/ckeditor.rb
+++ t/ckeditor.rb
@@ -1,7 +1,7 @@
 file 'config/initializers/ckeditor.rb', <<-CODE
 Ckeditor::Rails.configure do |config|
   # default is nil for all languages, or set as %w[en zh]
-  config.assets_languages = nil
+  config.assets_languages = %w{ zh-cn zh en }
 
   # default is nil for all plugins,
   # or set as white list: %w[image link liststyle table tabletools]
